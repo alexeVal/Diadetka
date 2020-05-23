043579b31ad6906dc11bc37406756642da2db4d8
alexeVal <62025563+alexeVal@users.noreply.github.com>
2020-05-23 20:30:26 +0300

    добавлено скачивание базы, вк оповещения

--- app/src/main/java/com/example/myapplication/Food_jornal.java
+++ app/src/main/java/com/example/myapplication/Food_jornal.java
@@ -1,39 +1,26 @@
 package com.example.myapplication;
 
-
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-
-import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SearchView;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
 import com.google.gson.Gson;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Base64;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
-import java.util.concurrent.ExecutionException;
-
 
 public class Food_jornal extends AppCompatActivity implements View.OnClickListener {
 
@@ -45,10 +32,9 @@ public class Food_jornal extends AppCompatActivity implements View.OnClickListen
     private VK_ID_base vkIdBase;
     private MyAdapter adapter;
     private Application_server server;
-    private FileLoading loader;
+    private boolean isReaded = false;
+    private Food food;
 
-    //food = gsonС.fromJson(read,Food.class);
-    //db.insert(food.getName(),food.getUrl(),food.getXe());
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -60,15 +46,31 @@ public class Food_jornal extends AppCompatActivity implements View.OnClickListen
 
         listView = findViewById(R.id.list);
 
+        Gson gson = new Gson();
+
+
+        Log.d("Тест","конец");
+
         server = new Application_server(getApplicationContext());
+
+        server.DownloadBase();
+
+        gsonToFood(readBase());
 
         food_list_db = new Food_list_db(getApplicationContext());
 
         db = new Food_db(getApplicationContext());
 
-        fulling_base();
+        for(int i = 1; i < readBase().size();i++){
+            String reads = readBase().get(i);
+            food = gson.fromJson(reads,Food.class);
+            db.insert(food.getName(),food.getUrl(),food.getXe());
+            Log.d("Тест",food.getName());
+        }
 
         adapter = new MyAdapter(this, db.selectAll());
+
+        fulling_base();
 
         listView.setAdapter(adapter);
 
@@ -100,28 +102,33 @@ public class Food_jornal extends AppCompatActivity implements View.OnClickListen
         startActivity(intent);
         finish();
     }
+
     public void fulling_base() {
-        File file = new File(getExternalCacheDir(), "base.txt");
-        String read = "";
-        Gson gsonС = new Gson();
-        Food food;
         if (db.selectAll().size() == 0) { //если база продуктов пуста
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(Food_jornal.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
             }
-            server.DownloadBase();
-            try {
-                FileInputStream in = new FileInputStream(file);
-                Scanner scanner = new Scanner(in);
-                while (scanner.hasNext()){
-                    read = scanner.nextLine();
-                    Log.d("Прочитанный файл",read);
-                }
-
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
         }
+    }
+    public ArrayList<String> readBase() {
+        String read = " ";
+        File file = new File(getExternalCacheDir(), "base.txt");
+        ArrayList<String> fileRading = new ArrayList<>();
+        try {
+            FileInputStream in = new FileInputStream(file);
+            Scanner scanner = new Scanner(in);
+            while (scanner.hasNext()) {
+                read = scanner.nextLine();
+                Log.d("Прочитанный файл", read);
+                fileRading.add(read);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return fileRading;
+    }
+    public void gsonToFood(ArrayList<String> gsons){
+
     }
 }
 
